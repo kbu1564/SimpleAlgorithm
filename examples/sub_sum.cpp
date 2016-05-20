079dc5664201ac0c2d0009ac3b130846d8f4565f
Byeoungwook Kim <quddnr145@gmail.com>
2016-05-21 04:14:46 +0900

    fix indentation to sub_sub example code

--- examples/sub_sum.cpp
+++ examples/sub_sum.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	int SUM[10001] = { 0, };
+  int SUM[10001] = { 0, };
 
-	// 입력이 0 ~ 10000까지 순차적으로 주어졌다고 가정
-	int INPUT[10001] = { 0, };
-	for (int i = 0; i < 10001; i++) INPUT[i] = i;
+  // 입력이 0 ~ 10000까지 순차적으로 주어졌다고 가정
+  int INPUT[10001] = { 0, };
+  for (int i = 0; i < 10001; i++) INPUT[i] = i;
 
-	int total = 0;
-	for (int i = 0; i < 10001; i++) SUM[i] = (total += INPUT[i]);
+  int total = 0;
+  for (int i = 0; i < 10001; i++) SUM[i] = (total += INPUT[i]);
 
-	int a = 5, b = 499; // 5 ~ 499 까지의 부분합 구하기 O(1)
-	int ans = SUM[b] - SUM[a-1];
+  int a = 5, b = 499; // 5 ~ 499 까지의 부분합 구하기 O(1)
+  int ans = SUM[b] - SUM[a-1];
 
-	cout << ans << endl;
-	return 0;
+  cout << ans << endl;
+  return 0;
 }
